05990aadfb24748208d66fbe57d53234968a09e0
Long Hoàng <longhoang0304@users.noreply.github.com>
2018-09-11 23:04:57 +0700

    Done train

--- station/station.cpp
+++ station/station.cpp
@@ -7,26 +7,79 @@
 Servo servo;
 RF24 stationRf(CE, CS);
 
-static const byte stationAddress[5] = {'s','t','a','t','n'};
-static  const byte trainAddress[5] = {'t','r','a','i','n'};
+static const byte stationAddress[5] = {'s', 't', 'a', 't', 'n'};
+static const byte trainAddress[5] = {'t', 'r', 'a', 'i', 'n'};
+static const byte dataToSend = 1;
+static double dataReceived[2];
+static bool newData = false;
 
-void controlServo(int angle) {
+void controlServo(int angle)
+{
   servo.write(angle);
 }
 
-void setupRf() {
-  servo.begin();
-  servo.setDataRate(RF24_250KBPS);
-  servo.openWritingPipe(stationAddress);
-  servo.openReadingPipe(1, trainAddress);
-}
-
-void setup_station() {
+void setupServo()
+{
   servo.attach(BAR_PIN);
   controlServo(0);
+}
+
+void setupRf()
+{
+  stationRf.begin();
+  stationRf.setDataRate(RF24_250KBPS);
+  stationRf.openWritingPipe(stationAddress);
+  stationRf.openReadingPipe(1, trainAddress);
+  stationRf.setRetries(3, 5);
+}
+
+void setup_station()
+{
+  pinMode(EMER_BTN, INPUT);
+  setupServo();
   setupRf();
 }
 
-void loop_station() {
+void alertTrain()
+{
+  stationRf.stopListening();
+  bool rslt;
+  rslt = stationRf.write(&dataToSend, sizeof(dataToSend));
+  stationRf.startListening();
 
+  if (rslt)
+  {
+    Serial.println("Acknowledge Received");
+  }
+  else
+  {
+    Serial.println("Tx failed");
+  }
+  Serial.println();
+}
+
+void getData()
+{
+  if (stationRf.available())
+  {
+    stationRf.read(&dataReceived, sizeof(dataReceived));
+    newData = true;
+  }
+}
+
+void handleReceivedData() {
+  newData = false;
+  // control light here
+}
+
+void loop_station()
+{
+  if(digitalRead(EMER_BTN)) {
+    alertTrain();
+  }
+  getData();
+  if (newData) {
+    handleReceivedData();
+  }
+  delay(1000);
 }
--- train/train.cpp
+++ train/train.cpp
@@ -10,6 +10,10 @@ static const byte trainAddress[5] = {'t','r','a','i','n'};
 static const int GAP_SIZE = 500; // assume that 500m
 static const int DIST_TO_STATION = 10000; // assum that 10km
 
+static byte dataReceived = 0;
+static double dataToSend[2];
+static bool newData = false;
+
 void setupRf() {
   servo.begin();
   servo.setDataRate(RF24_250KBPS);
@@ -19,6 +23,7 @@ void setupRf() {
 
 void setup_train() {
   pinMode(HALL_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
   setupRf();
 }
 
@@ -49,9 +54,50 @@ double calculateSpeed() {
   return -1;
 }
 
+void sendInfoToStation()
+{
+  stationRf.stopListening();
+  bool rslt;
+  rslt = stationRf.write(&dataToSend, sizeof(dataToSend));
+  stationRf.startListening();
+
+  if (rslt)
+  {
+    Serial.println("Acknowledge Received");
+  }
+  else
+  {
+    Serial.println("Tx failed");
+  }
+  Serial.println();
+}
+
+void getData()
+{
+  if (stationRf.available())
+  {
+    stationRf.read(&dataReceived, sizeof(dataReceived));
+    newData = true;
+  }
+}
+
+void handleReceivedData() {
+  newData = false;
+  digitalWrite(LED_PIN, 1);
+}
+
 void loop_train() {
   double speed = calculateSpeed();
   if (speed > 0) {
     double t = DIST_TO_STATION / speed;
+    dataToSend[0] = speed;
+    dataToSend[1] = t;
+    sendInfoToStation();
+    speed = -1;
   }
+  getData();
+  if (newData) {
+    handleReceivedData();
+  }
+  delay(50);
 }
